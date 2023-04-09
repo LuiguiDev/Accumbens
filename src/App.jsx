1eda3e1837f870dc92ea2a7bb8962312eeff5cab
Luis Rodríguez <lfrz.arc@gmail.com>
2023-04-08 23:36:18 -0600

    First version of progress chart, looks pretty decent

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ import { v4 as uuidv4 } from 'uuid';
 import { Day } from './Components/Day';
 import { Header } from './Components/Header';
 import { Footer } from './Components/Footer';
+import { AreaChart, ResponsiveContainer, Area, XAxis, Tooltip } from 'recharts';
 
 function App() {
   const initialState = JSON.parse(localStorage.getItem('achivements')) || [];
   const [achivements, setAchivements] = useState(initialState);
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    const newData = structuredClone(data)
+
+    achivements.forEach(day => {
+      newData.push({
+        date: day.date,
+        value: day.content.length
+      }
+      )
+      setData(newData)
+    })
+
+    console.log(newData)
+  }, [achivements])
 
   function changeAccState (newState) {
     setAchivements(newState);
@@ -26,6 +43,21 @@ function App() {
         id={uuidv4()}
         changeAccState={changeAccState}
       />
+      
+      <ResponsiveContainer width='100%' height={200}>
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id='color' x1='0' y1='0' x2='0' y2='1'  >
+              <stop offset='0%' stopColor='#09b276' stopOpacity={0.4} />
+              <stop offset='75%' stopColor='#09b276' stopOpacity={0.1} />
+            </linearGradient>
+          </defs>
+          <Area dataKey="value" stroke="#09b276" fill='url(#color)' />
+          <XAxis dataKey="date" />
+          <Tooltip />
+        </AreaChart>
+      </ResponsiveContainer>
+
       <Footer />
     </>
   )
